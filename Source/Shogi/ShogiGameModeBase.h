05bb15bef8964fff3648e37a9a044c50ca7c3b3e
KULINICH <ksihimyakich@gmail.com>
2023-05-17 22:45:49 +0300

    INIT

---
+++ Source/Shogi/ShogiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShogiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOGI_API AShogiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
